1fa790373ed7b0fd1164deb73378658a40e78dae
hjuren <hjuren02@gmail.com>
2022-05-16 10:14:55 +0200

    zmiana rozszerze

---
+++ src/components/Product.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Product(props) {
+	const { product } = props;
+	return (
+		<React.Fragment>
+			Product{{ product }};
+			<div>
+		  
+			<img className="small" src={product.image} alt={product.name}></img>
+			<h3>
+				{product.name}
+			</h3>
+			<div>${product.price}  </div>
+		</div>
+			<button>Add To Cart</button>
+			</React.Fragment>
+
+	);
+}
