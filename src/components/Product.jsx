c8c3c7b57096979ddf0d30cd28482170fe350d9b
hjuren <hjuren02@gmail.com>
2022-05-24 12:23:04 +0200

    new button class

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -9,7 +9,7 @@ export default function Product(props) {
                 <h3>{product.name}</h3>
                 <div>${product.price}</div>
             </div>
-            <button>Add To Cart</button>
+            <button class="button is-danger">Add To Cart</button>
         </React.Fragment>
     );
 }
